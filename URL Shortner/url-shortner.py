1d0c6589d85d93456e7304a93eff7f5aa4947023
Mehreen Mallick Fiona <155657140+Noblesse013@users.noreply.github.com>
2024-03-29 01:11:13 +0600

    Update url-shortner.py

--- URL Shortner/url-shortner.py
+++ URL Shortner/url-shortner.py
@@ -1,1 +1,10 @@
+import pyshorteners
 
+# Get the targetted URL from the user
+long_url = input("Pleas enter your long URL: ")
+
+# Shortening the URL using pyshorteners
+s = pyshorteners.Shortener()
+short_url = s.tinyurl.short(long_url)
+
+print(short_url)
